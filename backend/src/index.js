e6310e7cee112d7be5d8e734266aeb86be4228a5
williamwjd <william.jose.wjd@gmail.com>
2020-01-14 09:07:10 -0300

    Criando a conexão com o banco de dados mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,16 @@
 const express=require('express')
+const mongoose=require('mongoose')
+
+require('dotenv').config()
 
 const server=express()
 
-const port=3000
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-onnlg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+const port=process.env.SERVER_PORT
 
 server.listen(port,()=>{
     console.log(`Servidor online na porta ${port}`)
