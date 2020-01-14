ed33ad60dcd7f9f39a210bf4aab5356a168ea1bd
williamwjd <william.jose.wjd@gmail.com>
2020-01-14 12:13:15 -0300

    Buscando devs em um raio de 10km com filtro por techs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
 const {Router}=require('express')
 const DevController=require('./controller/DevController')
+const SeachController=require('./controller/SeachController')
 
 const routes=Router()
 
 routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store)
+
+routes.get('/search', SeachController.index)
 
 module.exports=routes
