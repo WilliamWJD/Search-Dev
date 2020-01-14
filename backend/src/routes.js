0eb17b14f9b7a00ed1859dc2ecef4c8e63b5b154
williamwjd <william.jose.wjd@gmail.com>
2020-01-14 11:55:08 -0300

    Criando metodo para listar devs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const DevController=require('./controller/DevController')
 
 const routes=Router()
 
+routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store)
 
 module.exports=routes
