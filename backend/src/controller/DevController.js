f657d6b4790cbdc16124df7e08798d45aa502dc8
williamwjd <william.jose.wjd@gmail.com>
2020-01-14 11:47:23 -0300

    Criando o controller de Dev e o metodo para adicionar um Dev

---
+++ backend/src/controller/DevController.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+const Dev = require('../models/Dev')
+
+const DevController = {
+    async create(req, res) {
+        const { github_username, techs, latitude, longitude } = req.body
+
+        const response = await axios.get(`https://api.github.com/users/${github_username}`)
+
+        const { name = login, avatar_url, bio } = response.data
+
+        const techsArray = techs.split(',').map(tech => tech.trim())
+
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+        }
+
+        const dev = await Dev.create({
+            github_username, name, avatar_url, bio, techs: techsArray, location
+        })
+
+        res.json(dev)
+    }
+}
+
+module.exports=DevController
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,23 +1,8 @@
 const {Router}=require('express')
-const axios =require('axios')
-const Dev=require('./models/Dev')
+const DevController=require('./controller/DevController')
 
 const routes=Router()
 
-routes.post('/devs', async (req,res)=>{
-    const {github_username, techs}=req.body
-    
-    const response=await axios.get(`https://api.github.com/users/${github_username}`)
-
-    const {name=login, avatar_url, bio}=response.data
-
-    const techsArray=techs.split(',').map(tech=>tech.trim())
-
-    const dev=await Dev.create({
-        github_username,name,avatar_url,bio,techs:techsArray
-    })
-
-    res.json(dev)
-})
+routes.post('/devs', DevController.create)
 
 module.exports=routes
