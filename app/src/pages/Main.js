198dee6cac55fa000153c5a1d22fc96cc325e8a0
williamwjd <william.jose.wjd@gmail.com>
2020-01-16 14:31:55 -0300

    Retornando devs por techs

--- app/src/pages/Main.js
+++ app/src/pages/Main.js
@@ -9,6 +9,7 @@ import api from '../services/api'
 function Main({ navigation }) {
     const [devs, setDevs] = useState([])
     const [currentRegion, setCurrentRegion] = useState(null)
+    const [techs, setTechs]=useState('')
     // const [latitude, setLatitude]=useState('')
     // const [longitude, setLongitude]=useState('')
 
@@ -42,7 +43,7 @@ function Main({ navigation }) {
             params: {
                 latitude,
                 longitude,
-                techs: 'NodeJS'
+                techs
             }
         })
 
@@ -93,6 +94,8 @@ function Main({ navigation }) {
                     placeholderTextColor="#999"
                     autoCapitalize="words"
                     autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
                 />
                 <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
                     <MaterialIcons name="my-location" size={20} color="#fff" />
