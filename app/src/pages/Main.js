ad7018f63fde04d0ae7ba1e06bc90c96a126833b
williamwjd <william.jose.wjd@gmail.com>
2020-01-16 12:27:41 -0300

    Inserindo as informações de callout

--- app/src/pages/Main.js
+++ app/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
-import {StyleSheet, Image} from 'react-native'
-import MapView, {Marker} from 'react-native-maps'
+import {StyleSheet, Image, View, Text} from 'react-native'
+import MapView, {Marker, Callout} from 'react-native-maps'
 import {requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location'
 
 function Main(){
@@ -39,6 +39,13 @@ function Main(){
         <MapView initialRegion={currentRegion} style={styles.map}>
             <Marker coordinate={{latitude:-22.8420964, longitude:-47.1502522}}>
                 <Image style={styles.avatar} source={{uri:'https://avatars1.githubusercontent.com/u/31516475?s=460&v=4'}}/>
+                <Callout>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>William José Dias</Text>
+                        <Text style={styles.devBio}>CTO na @WJD Solutions, um amante e entusiasta por novas técnologias</Text>
+                        <Text style={styles.devTechs}>ReactJS, Node, Java, React Native</Text>
+                    </View>
+                </Callout>
             </Marker>
         </MapView>
     )
