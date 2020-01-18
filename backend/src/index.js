5dc1d38963cab8e12c2ff15cf9c00221912e1228
williamwjd <william.jose.wjd@gmail.com>
2020-01-18 09:45:47 -0300

    Configurando projeto para trabalhar com socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,25 @@
 const express=require('express')
 const mongoose=require('mongoose')
 const cors=require('cors')
+const http=require('http')
 const routes=require('./routes')
+const {setupWebsocket}=require('./websocket')
 
 require('dotenv').config()
 
-const server=express()
+const app=express()
+const server=http.Server(app)
+
+setupWebsocket(server)
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-onnlg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
 
-server.use(cors())
-server.use(express.json())
-server.use(routes)
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 const port=process.env.SERVER_PORT
 
