ed5b5f7310109a3884e80966380c01a021fa3069
williamwjd <william.jose.wjd@gmail.com>
2020-01-18 10:08:28 -0300

    Configurando o envio de parametros via webSocket para o backend

--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -3,7 +3,8 @@ const socketio=require('socket.io')
 exports.setupWebsocket=(server)=>{
     const io=socketio(server)
 
-    io.on('connection', socker=>{
-        console.log(socker.id)
+    io.on('connection', socket=>{
+        console.log(socket.id)
+        console.log(socket.handshake.query)
     })
 }
